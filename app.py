0f8e49fc908b4ddab21c71a0cfb4555ad18c6d17
CToopher <cristopher.canales5895@alumnos.udg.mx>
2024-09-12 12:31:39 -0600

    Agregar Datos A BD en AddHTML con Despliegue, Falta CP

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 #importamos la libreria mysql para la conexión
 import mysql.connector
-from scripts.Registrar import RegistrarDatos, AgregarDatos
+from scripts.Registrar import RegistrarDatos, AgregarDatos, ObtenerPaises, ObtenerEstados, ObtenerCiudades, ObtenerCodigosPostales, RegistrarPais, RegistrarEstado, RegistrarCiudad #,RegistrarCodigoPostal
 
 #Cristo :D
 
@@ -15,7 +15,10 @@ def index():
 #Página principal (index)
 @app.route('/paginaAdd', methods = ['GET'])
 def indexx():
-    return render_template('add.html')
+    OPaises = ObtenerPaises()
+    OEstados = ObtenerEstados()
+    OCiudades = ObtenerCiudades()
+    return render_template('add.html', paises = OPaises, estados = OEstados, ciudades = OCiudades)
 
 #formulario para registrar datos
 @app.route('/registrar_datos', methods = ['GET', 'POST'])
@@ -26,6 +29,37 @@ def RegDatos():
 @app.route('/agregar_datos', methods = ['GET', 'POST'])
 def AddDatos():
     return AgregarDatos()
+
+#registrar pais
+@app.route('/registrar_pais', methods = ['POST'])
+def RegPais():
+    RegistrarPais()
+    return redirect('/paginaAdd')
+
+#registrar estado
+@app.route('/registrar_estado', methods = ['POST'])
+def RegEstado():
+    RegistrarEstado()
+    return redirect('/paginaAdd')
+
+#registrar ciudad
+@app.route('/registrar_ciudad', methods = ['POST'])
+def RegCiudad():
+    RegistrarCiudad()
+    return redirect('/paginaAdd')
+"""
+#registrar codigo postal
+@app.route('/registrar_codigopostal', methods = ['POST'])
+def RegCodPostal():
+    RegistrarCodigoPostal()
+    return redirect('/paginaAdd')
+    """
+
+@app.route('/obtener_codigos_postales')
+def obtener_codigos_postales_endpoint():
+    ciudad_id = request.args.get('ciudad_id')
+    codigos_postales = ObtenerCodigosPostales(ciudad_id)
+    return jsonify({'codigos_postales': codigos_postales})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- scripts/Registrar.py
+++ scripts/Registrar.py
@@ -49,5 +49,184 @@ def RegistrarDatos():
         connection.close()
         return render_template('index.html')
 
+#registrar Pais
+def RegistrarPais():
+    if request.method == 'POST':
+        #Relación de variables
+        pais = request.form['pais']
+        connection = create_connection()
+        cursor = connection.cursor()
+
+        #ingresar pais
+        cursor.execute("INSERT INTO tpaises (Nombre) VALUES (%s)",(pais,))
+        connection.commit() #envia la decalraciòn al servidor de MySQL y confirma la transacciòn
+        #pais_id = cursor.lastrowid #devuelve el valor generado para el autoingramentable
+        connection.close()
+        return render_template('add.html')
+
+#registrar Estado
+def RegistrarEstado():
+    if request.method == 'POST':
+        #Relación de variables
+        pais_id = request.form['pais_Id']
+        estado = request.form['estado']
+        connection = create_connection()
+        cursor = connection.cursor()
+
+        #ingresar estado
+        cursor.execute("INSERT INTO testados (Nombre, TPaises_Id) VALUES (%s, %s)",(estado, pais_id))
+
+        connection.commit()
+        connection.close()
+        return render_template('add.html')
+
+#registrar Ciudad
+def RegistrarCiudad():
+    if request.method == 'POST':
+        #Relación de variables
+        estado_id = request.form['estado_Id']
+        ciudad_id = request.form['ciudad_Id']
+        connection = create_connection()
+        cursor = connection.cursor()
+
+        #ingresar ciudad
+        cursor.execute("INSERT INTO tciudades (Nombre, TEstados_Id) VALUES (%s, %s)",(ciudad_id, estado_id))
+
+        connection.commit()
+        connection.close()
+        return render_template('add.html')
+"""
+#registrar Codigo Postal
+def RegistrarCodigoPostal():
+    if request.method == 'POST':
+        #Relación de variables
+        codigoPostal = request.form['CodigoPostal']
+        ciudad_id = request.form['ciudad_Id']
+        connection = create_connection()
+        cursor = connection.cursor()
+
+        # 4. Insertar Código Postal
+        cursor.execute("INSERT INTO tcodigosp (Codigo, TCiudades_Id, TCiudades_TEstados_Id) VALUES (%s, %s, %s)", (codigoPostal, ciudad_id, estado_id))
+
+        connection.commit()
+        connection.close()
+        return render_template('add.html') """
+
 def AgregarDatos():
     return('add.html')
+
+#Mostrar las peticiones encoladas
+def Mlistausuarios():
+    UsuariosAc = UsuariosA(RegresaUsuariosG())
+    UsuariosIn = UsuariosI(RegresaUsuariosG())
+    ipsb = ipsB
+    # Colocamos la página principal
+    return render_template('listausuarios.html', UsuariosA = UsuariosAc, UsuariosI=UsuariosIn, ipsB=ipsb)
+
+def ObtenerPaises():
+    """Recupera todos los países de la base de datos."""
+    connection = create_connection()
+    if connection is None:
+        return None
+
+    try:
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM tpaises")
+        paises = cursor.fetchall()
+        return paises
+        #return [pais['Nombre'] for pais in paises]
+    except Error as e:
+        print(f"Error al ejecutar la consulta: {e}")
+        return None
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+def ObtenerEstados():
+    """Recupera todos los estados de la base de datos."""
+    connection = create_connection()
+    if connection is None:
+        return None
+
+    try:
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM testados")
+        estados = cursor.fetchall()
+        return estados
+        #return [estado['Nombre'] for estado in estados]
+    except Error as e:
+        print(f"Error al ejecutar la consulta: {e}")
+        return None
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+def ObtenerCiudades():
+    """Recupera todas las ciudades de la base de datos."""
+    connection = create_connection()
+    if connection is None:
+        return None
+
+    try:
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM tciudades")
+        ciudades = cursor.fetchall()
+        return ciudades
+    except Error as e:
+        print(f"Error al ejecutar la consulta: {e}")
+        return None
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+def ObtenerCodigosPostales(ciudad_id):
+    """Recupera los códigos postales que pertenecen a la ciudad con el ID proporcionado."""
+    connection = create_connection()
+    if connection is None:
+        return None
+
+    try:
+        cursor = connection.cursor(dictionary=True)
+        # Consulta SQL para obtener los códigos postales que coinciden con el id de la ciudad
+        query = """
+            SELECT Id, Codigo
+            FROM tcodigosp
+            WHERE TCiudades_Id = %s
+        """
+        cursor.execute(query, (ciudad_id,))
+        codigos_postales = cursor.fetchall()
+        return codigos_postales
+    except Error as e:
+        print(f"Error al ejecutar la consulta: {e}")
+        return None
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+def BuscarPais(name):
+    """Busca un país por nombre y devuelve su ID si se encuentra."""
+    connection = create_connection()
+    if connection is None:
+        return None
+
+    try:
+        cursor = connection.cursor(dictionary=True)
+        query = "SELECT id FROM tpaises WHERE Nombre = %s"
+        cursor.execute(query, (name,))
+        result = cursor.fetchone()
+        if result:
+            return result['id']
+        else:
+            print(f"El país '{name}' no se encuentra en la base de datos.")
+            return None
+    except Error as e:
+        print(f"Error al ejecutar la consulta: {e}")
+        return None
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
